2ca1550ada7f16f8bbdc19f3a872e85d30d4254b
Michelle Opanowicz <Michelle.Opanowicz@clasohlson.se>
2024-08-26 12:10:08 +0200

    picture and countdown

--- wayoutwoods/src/App.js
+++ wayoutwoods/src/App.js
@@ -1,27 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import walkinwoods from './img/walkinwoods.JPG';
+import welcome from './img/welcome.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/navbar.js'; 
 
-import React, { Component } from 'react';
+import React from 'react';
+import {Card, CardBody, ListGroup, ListGroupItem, CardLink, CardText, CardTitle} from 'reactstrap';
 
 import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 
-// class Example extends Component {
-//   render() {
-//     return <FlipClockCountdown to={new Date().getTime() + 24 * 3600 * 1000 + 5000} />;
-//   }
-// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hi guys!!!! WAY OUT WOODS in
-        </p>
+
+    <header>
+      
+      <div className="main-background" style={{ backgroundImage: `url(${walkinwoods})`, backgroundSize: 'cover' }}>
+
         <FlipClockCountdown to={new Date().getTime() + 24 * 3600 * 1000 + 5000} />
+      </div>
+
       </header>
-    </div>
+
+      
   );
 
 

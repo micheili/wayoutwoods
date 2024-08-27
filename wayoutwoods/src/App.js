5c673b1a624e6d410ccb37b6faf7e1c2672db490
Michelle Opanowicz <Michelle.Opanowicz@clasohlson.se>
2024-08-27 11:55:14 +0200

    flex resizing

--- wayoutwoods/src/App.js
+++ wayoutwoods/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import walkinwoods from './img/walkinwoods.JPG';
 import welcome from './img/welcome.png';
+import logo from './img/logo.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/navbar.js'; 
 
 import React from 'react';
-import {Card, CardBody, ListGroup, ListGroupItem, CardLink, CardText, CardTitle} from 'reactstrap';
+import {Row, Col, Container, Image} from 'reactstrap';
 
 import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
@@ -17,8 +18,38 @@ function App() {
     <header>
       
       <div className="main-background" style={{ backgroundImage: `url(${walkinwoods})`, backgroundSize: 'cover' }}>
+            
+      <Container fluid>
+        <Row>
+            <Col className="text-center"> 
+            <img className='logo'
+                roundedCircle
+                fluid
+                src={logo}
+            />
+            </Col>
+        </Row>
+</Container>
 
-        <FlipClockCountdown to={new Date().getTime() + 24 * 3600 * 1000 + 5000} />
+
+
+    <Container fluid>
+        <Row className="d-flex align-items-center justify-content-center">
+            <Col className="text-center d-flex align-items-center justify-content-center"> 
+            
+    <FlipClockCountdown to={new Date("September 6, 2024 18:00:00")} 
+        labels={['DAYS', 'HOURS', 'MINUTES', 'SECONDS']}
+        labelStyle={{ color:'white', fontSize: 10, fontWeight: 500, textTransform: 'uppercase' }}
+        digitBlockStyle={{ width: 30, height: 50, fontSize: 30 }}
+      
+        />
+
+            </Col>
+        </Row>
+</Container>
+
+
+
       </div>
 
       </header>

2544e85d700a855832c22e43e5798bb9a2337a71
Michelle Opanowicz <Michelle.Opanowicz@clasohlson.se>
2024-09-03 12:43:47 +0200

    card for map

--- wayoutwoods/src/map.js
+++ wayoutwoods/src/map.js
@@ -1,30 +1,42 @@
 import React from "react";
 import map from "./img/vråmap.png";
-import { Row, Col, Container } from "reactstrap";
-
-
+import {
+  Row,
+  Col,
+  Container,
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  CardImg
+} from "reactstrap";
 
 export default function VraMap() {
   return (
     <div>
+
+<Card className="my-2">
+    <CardImg
+      alt="Card image cap"
+      src={map}
+      top
+      width="100%"
+    />
+    <CardBody>
+    <CardTitle tag="h1">Find your way in Vrå</CardTitle>
+          <CardSubtitle className="mb-2 text-muted" tag="h6">
+          <a
+              className="google-maps"
+              href="https://maps.app.goo.gl/VgLUMssNkSXELTa4A"
+            >
+              <CardText><h4>Spots in Vrå on Google Maps</h4>
+             </CardText> </a>
+          </CardSubtitle>
         
-      <Container className="m-5" fluid>
-          <Row className="d-flex align-items-center justify-content-center">
-            <Col className="text-center d-flex align-items-center justify-content-center">
-            <a className="google-maps" href="https://maps.app.goo.gl/VgLUMssNkSXELTa4A"><h1>Find your way in Vrå</h1></a>    
-            </Col>
-          </Row>
-        </Container>
-          
-          <Container className="m-5" fluid>
-          <Row className="d-flex align-items-center justify-content-center">
-            <Col className="text-center d-flex align-items-center justify-content-center">
-            <img className="vrå-map"src={map}></img>
-    
-            </Col>
-          </Row>
-        </Container>
-     
-      </div>
+    </CardBody>
+  </Card>
+
+    </div>
   );
 }
